10a4c2ffa033f0f7322369744419107460f174ad
kencheng <kenny851017@gmail.com>
2016-11-13 06:17:32 -0500

    run flask on SJ

--- chrome_extension/flask/back_end.py
+++ chrome_extension/flask/back_end.py
@@ -8,7 +8,13 @@ import unicodedata
 from requests.utils import quote
 import urlparse
 import random
-from '../../train.py' import outputClass
+#import cPickle as pickle
+import nltk
+from sklearn.externals import joblib
+#from '../../prediction.py' import outputClass
+
+
+keyWords=[]
 
 app = Flask(__name__)
 
@@ -32,20 +38,42 @@ def give_num(link):
 		return "RIGHT"
 	return "LEFT"
 
-@app.route('/classify/<path:link>', methods=['GET'])
+@app.route('/classify/<path:link>')
 def classify(link):
 	#link is escaped
-	print(link)
+	#print(link)
+	#print(outputClass("for the second day in a row the liberal michael moore has published a plan for activists to put into motion whats getting all of the attention is item on the list pursuing the impeachment of donald trump its highly unlikely that the requisite minority of obedient house republican members would defend the constitution from a rampaging member of their own party but theoretically it could happen at any time with the number of lawsuits and charges leveled against trump across the country he also wants to ditch the electoral college which at this point makes sense considering that until it had only a failure rate to deliver the most voted candidate and since has failed to deliver the elected candidate to office of the time second list of recipes for the american left to move forward strictly an declaration either beyond proposing that a swift opposition movement emerge he also demanded a program of obstruction to deny supreme court judges reform within the democratic national committee and even an apology to sanders from the party the most immediately doable item on the list would be a special prosecutor to investigate fbi director james partisan interference in this years election theres already a movement to terminate the fbi directors employment for violating the hatch act if there could be a yearlong server investigation surely the government does have the resources to investigate its top lawman after the justice departments office of special counsel rules on the hatch act complaints wish list ended with the national and the personal he called for major changes to our voting systems for federal elections to ensure an accountable paper trail a holiday on election day and to end the disenfranchisement of felons who have completed their sentences heres all of them must quickly and decisively form an opposition movement the likes of which been seen since the will do my part to help lead this as sure many others elizabeth warren the community will too the core of this opposition force will be fueled by young people who as with occupy wall street and black lives matter tolerate and are relentless in their resistance to authority they have no interest in compromising with racists and misogynists prepare to impeach trump just as the republicans were already planning to do with president from day one we must organize the apparatus that will bring charges against him when he violates his oath and breaks the law and then we must remove him from office must commit right now to a vigorous fight civil disobedience if necessary which will block any and all donald trump supreme court nominees who do not meet our approval we demand the democrats in the senate aggressively filibuster any nominees who support citizens united or who oppose the rights of women immigrants and the poor this is demand the apologize to sanders for trying to fix the primaries against him for spinning the press to ignore his historic campaign for giving the questions in advance at the flint debate for its latent ageism and in trying to turn voters against him because of his age or religious beliefs and for its system of who are elected by no one we all know now had been given a fair shot he probably would have been the nominee and he as the true outsider and change candidate would have inspired and fired up the base and soundly defeated donald trump if no apology is soon forthcoming from the thats ok when we take over the democratic party yesterdays list we will issue the apology in person demand that president establish a special prosecutor to investigate who and what was behind fbi director james illegal interference into the presidential election days before the vote was held begin a national push while its fresh in mind for a constitutional amendment to fix our broken electoral system eliminate the electoral college popular vote only paper ballots only no electronic voting election day must be made a holiday for all or held on a weekend so more people vote all citizens regardless of any with the criminal justice system must have the right to vote swing states like florida and virginia of all black men are prohibited by law from convince president to immediately do what he should have done a year ago send in the army corps of engineers to flint to dig up and replace all the poisoned pipes nothing has changed the water in flint is still unusable"))
+	#return "LEFT"
 	page_text = get_text(link)
-	if text_is_conservative(text):
+	if text_is_conservative(page_text):
 		return "RIGHT"
 	return "LEFT"
 
 def text_is_conservative(text):
-	return True
+	print text
+	res = outputClass(text)
+	return res
+
+def outputClass(text):
+	global keyWords
+	buildKeyWords('Word_Counts.csv')
+	trainingVector=[0 for i in range(len(keyWords))]
+	clf=joblib.load('TrainedClassifier')
+	tokens = nltk.ngrams(nltk.wordpunct_tokenize(text.lower()),3)
+	totalWords = 0
+	for word in tokens:
+		if word[0].isalpha() and word[1].isalpha() and word[2].isalpha():
+			totalWords+=1
+			if word in keyWords:
+				trainingVector[keyWords.index(word)]+=1
+		
+	for i in range(len(trainingVector)):
+		if totalWords!=0:
+			trainingVector[i]=1+(trainingVector[i]/totalWords)
+
+	return clf.predict(trainingVector)[0]
 
 def get_text(link):
-
 	with open("english_words.txt") as word_file:
 		english_words = set(word.strip().lower() for word in word_file)
 
@@ -54,7 +82,7 @@ def get_text(link):
     # 	english_words = (set(word.strip.lower()))
 
 	#link = urlparse.unquote(link)
-	alchemy_language = AlchemyLanguageV1(api_key='b299a98dda5788d296ef0daa33bf74c54afff1f7')
+	alchemy_language = AlchemyLanguageV1(api_key='396444a062b0b8f61080b23a4b2b461e638bb3a3')
 	txt = json.dumps(alchemy_language.text(url=link),indent=0)
 	txt = json.loads(txt)
 	txt = txt['text']
@@ -73,4 +101,12 @@ def get_text(link):
 	ans = ' '.join(string)
 	return ans
 
+def buildKeyWords(keyWordsFileName):
+	global keyWords
+	f=open(keyWordsFileName,'r')
+	for line in f:
+		splits=line.split(',')
+		keyWords.append((splits[0],splits[1],splits[2]))
+
+	keyWords=keyWords[:500]
 
